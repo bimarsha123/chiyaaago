9bce7f1d1661a4d58fa5daafb45ae8cf367214aa
Bimarsha Mishra <141608360+bimarsha123@users.noreply.github.com>
2026-05-09 13:51:59 +0545

    Update settings.py

--- chiya_aago/settings.py
+++ chiya_aago/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "django-insecure-change-me-in-production-key-2024")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["chiyaaago.com","www.chiyaaago.com","127.0.0.1"]
 
